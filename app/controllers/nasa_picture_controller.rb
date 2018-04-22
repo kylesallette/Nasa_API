c5b42d873e518ca9ae7bd169cf3b4c5fd4f49c3d
kylesallette <kylesallette@gmail.com>
2018-04-22 14:59:04 -0600

    Precompiled assets

--- app/controllers/nasa_picture_controller.rb
+++ app/controllers/nasa_picture_controller.rb
@@ -2,6 +2,7 @@ class NasaPictureController < ApplicationController
 
   def index
     @nasa_info = Api.get_api_info
+      @media_type_is_video = @nasa_info['media_type'].eql?('video')
   end
 
 end
